779e965dc27903f1d7ddbf4f2dda3383133772b3
Pedro Elias <epedro520@gmail.com>
2021-10-12 21:00:41 -0300

    Upgrade:adding a Horizontal RecyclerView into MainActivity

---
+++ app/src/main/java/com/example/softcom_challenge/Adapter/HorizontalSlideAdapter.kt
@@ -0,0 +1,42 @@
+package br.com.francisco.projeto_final_PDMO.MyAdapter
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.softcom_challenge.Models.Sectors
+import com.example.softcom_challenge.R
+
+class  OperacoesAdapter(private val dataSet: ArrayList<Sectors>):
+    RecyclerView.Adapter<OperacoesAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView
+        var image: ImageView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            title = view.findViewById(R.id.TextView_Card)
+            image = view.findViewById(R.id.ImageView_Card)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.button_slide_sectors, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = dataSet[position].title
+        holder.image = dataSet[position].image
+    }
+}
---
+++ app/src/main/java/com/example/softcom_challenge/Models/Sectors.kt
@@ -0,0 +1,8 @@
+package com.example.softcom_challenge.Models
+
+import android.media.Image
+
+class Sectors (
+    var title:String,
+    var image:ImageView
+)
